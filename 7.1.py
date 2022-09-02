5793c03599dc354d4c5a66026f34b271b66313f2
caranchal <abrikospd2@mail.ru>
2022-09-02 07:58:31 +0700

    выводит наибольшее число через функцию

---
+++ 7.1.py
@@ -0,0 +1,11 @@
+def large(arr):
+    max_ = arr[0]
+    for ele in arr:
+        if ele > max_:
+           max_ = ele
+    return max_
+
+
+list1 = [1,4,5,2,6]
+result = large(list1)
+print(result)
