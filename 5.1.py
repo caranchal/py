63c8cea58b1cbb9015d99fd49691112ce3c2e342
caranchal <abrikospd2@mail.ru>
2022-09-02 07:34:30 +0700

    сумирует диапозон чисел

---
+++ 5.1.py
@@ -0,0 +1,11 @@
+import random
+def f(a,b):
+    a = 0
+    for i in range(x,y+1):
+        a += i
+        a = sum(range(x,y+1))
+    return a
+x = int(input("от"))
+y = int(input("до"))
+print(f(x,y))
+
