6a1f122497918b7f50c038619457135bdbf84ba1
caranchal <abrikospd2@mail.ru>
2022-09-06 19:56:56 +0700

    вывод диапозон чисел во 2 степень

--- 3.6.py
+++ 3.6.py
@@ -10,3 +10,16 @@ list1 = [x for x in powof2(10)]
 print(list)
 list2 = list(powof2(10))
 print(list2)
+
+
+def Fib(n):
+    p1 = p2 = 1
+    for i in range(n):
+        if i in [0,1]:
+            yield 1
+        else:
+            n = p1 + p2
+            p2,p1 = p1 , n
+            yield n
+fibsChels = list(Fib(10))
+print(fibsChels)
