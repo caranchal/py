46acc3ec0b7ed47ac68e6a0da76cf2ed50383b66
caranchal <abrikospd2@mail.ru>
2022-09-13 21:42:46 +0700

    вычисления количества строк, и символов в файле

--- вычисления количесвта строк, и символов в файле.py
+++ вычисления количесвта строк, и символов в файле.py
@@ -1,11 +1,15 @@
 from os import strerror
 import string
 try:
+    symbols = 0
     lines = 0
     s = open("C:/Users/Мк/Desktop/text.txt", "rt", encoding="UTF-8")
     for line in s:
         lines += 1
+        symbols += len(line.strip('\n'))
 
-    print("количество строк",lines)
+
+    print("количество строк:",lines,)
+    print("количество символов:", symbols)
 except IOError as err:
     print("I/O eror occured", strerror(err.errno))
