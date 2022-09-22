288be039ef5d8585f45a395491898d7959b5d625
caranchal <abrikospd2@mail.ru>
2022-09-22 21:52:32 +0700

    класс human

---
+++ human.py
@@ -0,0 +1,32 @@
+class bankData:
+    count = 0
+    def __init__(self, name1, name2, name3, birthday, country, city):
+        bankData.counte += 1
+        self.name1 = name1
+        self.name2 = name2
+        self.name3 = name3
+        self.birthday = birthday
+        self.country = country
+        self.city = city
+
+class city(bankData):
+
+
+class Human:
+    count = 0
+    def __init__(self):
+        count += 1
+        self.fio = fio
+        self.city = city
+    def print(ount):
+
+
+    def print(self, city):
+
+user = Human
+print(userprint.c)
+
+
+
+
+
